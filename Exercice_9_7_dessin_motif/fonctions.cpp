492a390f61a8732fb0b4c789cd3efc3b24eed8c1
Noanono <soler.noah@gmail.com>
2022-11-05 10:56:59 +0100

    fonctions triangle et triangle inverse

--- Exercice_9_7_dessin_motif/fonctions.cpp
+++ Exercice_9_7_dessin_motif/fonctions.cpp
@@ -1,15 +1,37 @@
 //
 // Created by soler on 05/11/2022.
 //
+#include <iostream>
+using namespace std;
 
 #include "fonctions.h"
 
 void Schema_triangle(int n){
-
+    int i = 1;
+    while(i <= n){
+        int k = 1;
+        while(k <= i){
+            cout << "*";
+            k++;
+        }
+        cout << endl;
+        i++;
+    }
+    cout << endl;
 }
 
 void Schema_inverse(int n){
-
+    int i = n;
+    while(i >= 1){
+        int k = i;
+        while(k >= 1){
+            cout << "*";
+            k--;
+        }
+        cout << endl;
+        i--;
+    }
+    cout << endl;
 }
 
 void Schema_sapin(int n){
